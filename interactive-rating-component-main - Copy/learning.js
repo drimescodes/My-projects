e37c2d0016ff64b98c8c52306b5b9c7a13c715c3
drimescodes <108256103+drimescodes@users.noreply.github.com>
2022-12-09 07:10:28 +0100

    Add files via upload
    Uploading my projects to show Mr Shaheb

---
+++ interactive-rating-component-main - Copy/learning.js
@@ -0,0 +1,480 @@
+"use strict"
+
+// let message
+// message = 'Javascript no hard, na una dey disguise';
+// message = 'omo, i never get am wella';
+
+
+
+
+
+// alert(message);
+
+
+// alert(
+//     "Hello, World!"
+// );
+
+// alert(
+//     3 +
+//     1
+//     + 2);
+// alert("not a number" / 2);
+
+// alert(Infinity
+// );
+
+// alert(
+//     `Hello, ${message}`
+// );
+
+
+
+// let isGreater= 4>1;
+
+// alert(isGreater);
+
+// let age = prompt("How old are you?", 200);
+
+// // change the value to undefined
+// // age = undefined;
+
+// alert(`You are ${age} years old!`); 
+// // 
+
+// let isBoss = confirm("Are you the boss?");
+
+// alert(isBoss); 
+
+
+
+
+
+
+// let user = 'John';
+// let age = 25;
+// let hello = 'Hello World';
+
+// alert(user);
+// alert(age);
+
+// alert(
+//     hello
+// );
+
+
+
+
+// +2347066396709
+
+//     document.getElementById("demo").innerHTML =
+// "Hello Dolly!";
+
+// function myFunction() {
+//     document.getElementById("demo1").innerHTML = "Hello Dolly!";
+//     document.getElementById("demo2").innerHTML = "How are you?";
+//   }
+
+
+// result = prompt(title, [default]);
+
+
+
+{
+    // let message="hello world"
+    //     alert(message);
+}
+
+
+// let message
+// message="WTF"
+
+// alert('g'/2);
+
+// let a = prompt ("First number?", 1);
+// let b = prompt("Second number?", 2);
+
+// alert(+a + +b);
+
+
+
+
+/*
+alert( 0 === false );
+
+let day = prompt('What day did God created man');
+
+if (day > 6){
+   alert( 'Too Far' )
+
+} else if (day < 6){
+    alert( 'Too Short' )
+} else {
+    alert( 'You got it correctly' )
+
+
+};
+*/
+
+
+
+// let name = prompt( "What's the “official” name of JavaScript?" )
+
+// if ( name == 'EcmaScript') {
+//     alert( 'Right')
+// } else {
+//     alert( "You don't know? “ECMAScript”!" )
+// };
+
+
+
+// let input = prompt( 'INPUT A NUMBER' );
+
+// if ( input > 0){
+//     alert( 1 );
+// } else if ( input < 0){
+//     alert(-1);
+// } else if ( input == 0){
+//     alert(0);
+// }
+
+// let result = (a + b < 4) ? 'Below' : 'Over';
+
+// let message = (login == 'Employee') ? 'Hello' :
+//   (login == 'Director') ? 'Greetings' :
+//   (login == '') ? 'No login' :
+//   '';
+
+
+// console.log('hELLO World');
+// const string4 = new String("A String object");
+
+
+/*
+let userName = prompt( "Who's there?", '' );
+if ( userName === 'Admin' ){
+
+let pass = prompt( 'Password?', '' );{
+    if ( pass === 'TheMaster' ){
+        alert( 'Welcome!' );
+    } else if (pass === '' || pass === null){
+        alert('Canceled');
+    } else {
+        alert("I don't know you");
+    }
+
+}
+} else if ( userName === '' || userName === null){
+    alert('Canceled');
+} else {
+    alert("I don't know you");
+};
+*/
+
+
+// switch ('Bananas') {
+//     case 'Oranges':
+//       console.log('Oranges are $0.59 a pound.');
+//       break;
+//     case 'Apples':
+//       console.log('Apples are $0.32 a pound.');
+//       break;
+//     case 'Bananas':
+//       console.log('Bananas are $0.48 a pound.');
+//       break;
+//     case 'Cherries':
+//       console.log('Cherries are $3.00 a pound.');
+//       break;
+//     case 'Mangoes':
+//     case 'Papayas':
+//       console.log('Mangoes and papayas are $2.79 a pound.');
+//       break;
+//     default:
+//       console.log(`Sorry, we are out of ${expr}.`);
+//   }
+
+//   console.log("Is there anything else you'd like?");
+
+
+
+// const foo = 3;
+// let output = 'Output: ';
+// switch (foo) {
+//   case 0:
+//     output += 'So ';
+//   case 1:
+//     output += 'What ';
+//     output += 'Is ';
+//   case 2:
+//     output += 'Your ';
+//   case 3:
+//     output += 'Name';
+//   case 4:
+//     output += '?';
+//     console.log(output);
+//     break;
+//   case 5:
+//     output += '!';
+//     console.log(output);
+//     break;
+//   default:
+
+// }
+
+// let i = 2;
+// while (i < 5){
+//     alert(i);
+//     i++
+// }
+// for(let i = 2; i < 5; i++){
+//     alert(i);
+// }
+// for (let i = 1; i <= 10; i++){
+//     if (i % 2 !== 0) continue;
+//     console.log(i);
+// }
+
+
+// let i = 0;
+// while( i < 3){
+//     console.log(`number ${i}!`);
+//     i++ 
+
+// }
+
+
+
+// let input 
+// input = prompt("A number greater than 100", "");
+// if (input > 100){
+//     alert("Congrats");
+// } else for ( input=prompt; prompt <= 100 && num; ) {
+
+//     prompt("A number greater than 100", "");
+// };
+
+// let num;
+// do {
+//     num = prompt("Enter a number greater than 100?", 0);
+// }while (num <= 100 && num)
+
+
+// let n = 10;
+// output:
+// for( let i = 2; i <= 10; i++){
+//     for( let j = 1; j < i; j++){
+//         if(i % j ==0) continue output
+//         console.log(i);
+//     }
+
+// } 
+// let place
+// switch(place) {
+//     case 'first':
+//         console.log('Gold');
+//           break;
+
+//     case 'second':
+//         console.log('Silver');
+//           break;
+//          case 'third':
+//         console.log('Bronze');
+//           break;
+
+//     default:
+//         console.log("No Medal");
+// }
+
+// COURSERA EXERCISE
+/*
+var age = 19;
+if (age >= 65) {
+    console.log("Werey dey get income from pension")
+} else if (age < 65 && age >= 18) {
+    console.log("Werey your salary go soon finish")
+} else if (age < 18){
+    console.log("Go find Worker, you still dey collect money from your mama")
+} else {
+    console.log("werey put number for something")
+}
+8
+*/
+
+// Code the days of the program as a switch statement  //
+// let day = 'Saturday';
+
+// switch (day) {
+//     case 'Monday':
+//         console.log("Get a job")
+//         break;
+
+//     case 'Tuesday':
+//         console.log("Got a job? Then go to work")
+//         break;
+
+//     case 'Wednessday':
+//         console.log("Make sure you woo a lady before the weekend")
+//         break;
+
+//     case 'Thursday':
+//         console.log("Get the lady to a talking stage schedule tomorrow as a date")
+//         break;
+
+//         case 'Friday':
+//        console.log("After closing hours take her to the date and spend the night with her") 
+//         break;
+
+//         case 'Saturday':
+//        if (prompt("Did she sleep over?", 'only yes/no is the accepted answer') = true) {
+//         console.log('Spend the whole day with her')
+//        } else {
+//         console.log('Just do laudry and tidy your dirty house')
+//        }
+//         break;
+
+//         case 'Sunday':
+//        console.log("REST ") 
+//         break;
+//     default:
+//         break;
+// }
+
+
+
+// let day = Tuesday;
+
+
+
+// switch (day) {
+//     case 'Monday':
+//         alert("Get a job")
+//         break;
+
+//     case 'Tuesday':
+//         alert("Got a job? Then go to work")
+//         break;
+
+//     case 'Wednessday':
+//         alert("Make sure you woo a lady before the weekend")
+//         break;
+
+//     case 'Thursday':
+//         alert("Get the lady to a talking stage schedule tomorrow as a date")
+//         break;
+
+//         case 'Friday':
+//        alert("After closing hours take her to the date and spend the night with her") 
+//         break;
+
+//         case 'Saturday':
+//        if (prompt("Did she sleep over?", 'only yes/no is the accepted answer') = true) {
+//         alert('Spend the whole day with her')
+//        } else {
+//         alert('Just do laudry and tidy your dirty house')
+//        }
+//         break;
+
+//         case 'Sunday':
+//        alert("REST ") 
+//         break;
+//     default:
+//         break;
+// }
+
+
+// let i = prompt("Type start to get started", "start")
+
+// outer: for (i = 1; i <= 5; i++) {
+
+//     switch (i) {
+//         case 1:
+//             let p = prompt("What year were you born", "2004")
+//             if (2022 - p >= 18) {
+//                 alert('welcome');
+//                 continue outer;
+//             } else {
+//                 alert(`You are ${2022 - p} years, The minimum requirement is 18`, "")
+//             }
+//             break;
+
+
+//         case 2:
+//             let q = prompt("What was Meta Platforms inc formerly known as", "NB:" + " All answers must be in lowercase")
+//             if (q === "facebook") {
+//                 alert('Correct,' + " Unto the next one");
+//                 continue outer;
+//             } else {
+//                 alert("Wrong");
+//                 break;
+//             }
+
+//         case 3:
+//             let r = prompt("Which English city is known as steel city? ", "NB:" + " All answers must be in lowercase")
+//             if (r === "sheffield") {
+//                 alert('Correct,' + " Unto the next one");
+//                 continue outer;
+//             } else {
+//                 alert("Wrong");
+//                 break;
+//             }
+
+//         case 4:
+//             let s = prompt("Which former british colony was given back to china in 1997", "NB:" + " All answers must be in lowercase")
+//             if (s === "hong kong") {
+//                 alert('Correct,' + " Unto the next one");
+//                 continue outer;
+//             } else {
+//                 alert("Wrong");
+//                 break;
+//             }
+//         case 5:
+//             let t = prompt("Which element is said to keep bones strong?", "NB:" + " All answers must be in lowercase")
+//             if (t === "calcium") {
+//                 alert('Correct,' + " Unto the next one");
+//                 continue outer;
+//             } else {
+//                 alert("Wrong");
+//                 break;
+//             }
+//         default:
+//             alert('You got 0 correctly')
+//             break;
+//     }
+
+
+// }
+// alert("Genius");
+
+// min(2, 5);
+// min(3, -1);
+// min(2, 2)
+// function min (a, b){
+//     if((a-b)>0){
+//         return console.log (`The lesser one is ${b}`)
+    
+//     } else {
+//         return console.log (`The lesser one is ${a}`)
+//     }
+// }
+// function min (a, b){
+// return ((a-b)>0) ?console.log(`The lesser one is ${b}`): console.log (`The lesser one is ${a}`);
+// }
+{
+let x = prompt("input x");
+let n = prompt("input n");
+
+
+alert(pow(x, n));
+
+}
+
+function pow(x, n) {
+    return (x ** n);
+}
+
+
+
+
+
+
+
